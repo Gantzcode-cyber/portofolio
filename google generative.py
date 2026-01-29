8bc5a5febeea6402a9abca7bc7fee27de7784ce2
Gantzcode-cyber <gantzcode@gmail.com>
2026-01-29 21:51:15 +0700

    Update google generative.py

--- google generative.py
+++ google generative.py
@@ -103,7 +103,7 @@ try:
         st.stop()
 except: st.stop()
 
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('moduls/gemini-2.5-flash')
 if "messages" not in st.session_state: st.session_state.messages = []
 
 # --- 5. SIDEBAR NAVIGASI ---
